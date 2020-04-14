eb4febc2eca1010c09ccfb70f4ee61314c18d386
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-14 10:18:13 +0900

    (update) admin site path

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(os.environ.get('ADMIN_SITE_PATH'), admin.site.urls),
 
     path('', include('home.urls')),
     path('words/', include('words.urls')),
