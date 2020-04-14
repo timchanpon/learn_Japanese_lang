4073e8b9fa2ccda3b251435e61b7177fdffc7212
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-14 09:35:21 +0900

    (add) add chars dictionary

---
+++ words/chars_dict.py
@@ -0,0 +1,104 @@
+hiragana = {
+
+    # vowel
+    'あ': 'a',
+    'い': 'i',
+    'う': 'u',
+    'え': 'e',
+    'お': 'o',
+
+    # consonant K
+    'か': 'ka',
+    'き': 'ki',
+    'く': 'ku',
+    'け': 'ke',
+    'こ': 'ko',
+
+    # consonant K"
+    'が': 'ga',
+    'ぎ': 'gi',
+    'ぐ': 'gu',
+    'げ': 'ge',
+    'ご': 'go',
+
+    # consonant S
+    'さ': 'sa',
+    'し': 'si',
+    'す': 'su',
+    'せ': 'se',
+    'そ': 'so',
+
+    # consonant S"
+    'ざ': 'za',
+    'じ': 'ji',
+    'ず': 'zu',
+    'ぜ': 'ze',
+    'ぞ': 'zo',
+
+    # consonant T
+    'た': 'ta',
+    'ち': 'chi',
+    'つ': 'tsu',
+    'て': 'te',
+    'と': 'to',
+
+    # consonant T"
+    'だ': 'da',
+    'ぢ': 'di',
+    'づ': 'du',
+    'で': 'de',
+    'ど': 'do',
+
+    # consonant N
+    'な': 'na',
+    'に': 'ni',
+    'ぬ': 'nu',
+    'ね': 'ne',
+    'の': 'no',
+
+    # consonant H
+    'は': 'ha',
+    'ひ': 'hi',
+    'ふ': 'hu',
+    'へ': 'he',
+    'ほ': 'ho',
+
+    # consonant H'
+    'ぱ': 'pa',
+    'ぴ': 'pi',
+    'ぷ': 'pu',
+    'ぺ': 'pe',
+    'ぽ': 'po',
+
+    # consonant H"
+    'ば': 'ba',
+    'び': 'bi',
+    'ぶ': 'bu',
+    'べ': 'be',
+    'ぼ': 'bo',
+
+    # consonant M
+    'ま': 'ma',
+    'み': 'mi',
+    'む': 'mu',
+    'め': 'me',
+    'も': 'mo',
+
+    # consonant Y
+    'や': 'ya',
+    'ゆ': 'yu',
+    'よ': 'yo',
+
+    # consonant R
+    'ら': 'ra',
+    'り': 'ri',
+    'る': 'ru',
+    'れ': 're',
+    'ろ': 'ro',
+
+    # consonant W
+    'わ': 'wa',
+    'を': 'wo',
+    'ん': 'n',
+
+}
