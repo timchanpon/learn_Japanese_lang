a0e83c36864bc36e06e67f2c75101825fccd83ef
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-15 08:38:43 +0900

    (update) production setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,3 +5,29 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+
+
+# Register static root
+STATIC_ROOT = '/usr/share/nginx/html/static'
+
+
+# Security settings
+# security.W006
+SECURE_CONTENT_TYPE_NOSNIFF = True
+# security.W007
+SECURE_BROWSER_XSS_FILTER = True
+# security.W019
+X_FRAME_OPTIONS = 'DENY'
+
+# Security settings for https
+# security.W004
+# SECURITY_HSTS_SECONDS = 31536000
+# SECURITY_HSTS_INCLUDE_SUBDOMAINS = True
+# security.W008
+# SECURE_SSL_REDIRECT = True
+# security.W012
+# SESSION_COOKIE_SECURE = True
+# security.W016
+# CSRF_COOKIE_SECURE = True
+# security.W021
+# SECURE_HSTS_PRELOAD = True
