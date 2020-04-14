04b79ed62f3ca6f245d8fae64cfa2587a398fc51
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-14 09:33:06 +0900

    (add) add word model

--- words/models.py
+++ words/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class Word(models.Model):
+
+    word = models.CharField(max_length=15, unique=True)
+    meaning_id = models.CharField(max_length=30, blank=True, null=True)
+    other = models.TextField(max_length=150, blank=True, null=True)
+
+    romaji = models.CharField(max_length=60, blank=True, null=True)
+    word_count = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.word
