3185851899df1a724c99be3ce7589ed133410c4c
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-14 09:38:36 +0900

    (add) register word model to admin

--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Word
+from .chars_dict import hiragana
+
+
+class WordAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        word = obj.word
+
+        # romaji
+        romaji = ''
+        for char in word:
+            if char in hiragana:
+                romaji += hiragana[char] + '-'
+            else:
+                romaji = 'ERROR_'
+                break
+        romaji = romaji[:-1]
+
+        # word_count
+        word_count = len(word)
+
+        obj.spell = romaji
+        obj.word_count = word_count
+        obj.save()
+
+
+admin.site.register(Word, WordAdmin)
