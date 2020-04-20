a862278fe72b07375505b3a3702540401d91bf5c
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-20 20:09:43 +0900

    (add) Character serializer

--- apps/words/serializers.py
+++ apps/words/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Word
+from .models import Word, Character
 
 
 class WordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Word
         fields = ('pk', 'word', 'romaji', )
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = ('pk', 'hiragana', 'romaji', )
