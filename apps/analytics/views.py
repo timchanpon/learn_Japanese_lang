20b1dbda6d7460900b1e07d298faa400f184399c
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-21 08:40:30 +0900

    (add) post to AnalyticsAPIView

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Analytics
 
 
 class AnalyticsAPIView(APIView):
@@ -16,3 +19,22 @@ class AnalyticsAPIView(APIView):
         }
 
         return Response(data)
+
+    def post(self, request):
+        forwarded_address = self.request.META.get('HTTP_X_FORWARDED_FOR')
+        if forwarded_address:
+            client_address = forwarded_address.split(',')[0]
+        else:
+            client_address = request.META.get('REMOTE_ADDR')
+
+        ips = Analytics.objects.values_list('ip_address', flat=True)
+        ips_list = list(ips)
+
+        if client_address in ips_list:
+            obj = Analytics.objects.get(ip_address=client_address)
+            obj.play_time += 1
+            obj.save()
+        else:
+            Analytics.objects.create(ip_address=client_address)
+
+        return Response(status=status.HTTP_200_OK)
