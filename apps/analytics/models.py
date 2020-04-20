c6c2a5057aa179229f0ee362e6a66c363803966d
timchanpon <62697984+timchanpon@users.noreply.github.com>
2020-04-21 04:08:29 +0900

    (create) Analytics model

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class Analytics(models.Model):
+
+    ip_address = models.CharField(max_length=15, unique=True)
+    play_time = models.PositiveIntegerField(default=1)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.ip_address
